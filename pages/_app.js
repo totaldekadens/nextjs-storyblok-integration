fe9ca357062b56a43212013d4fa30fdbb58dea38
totaldekadens <angelica.moberg@hotmail.com>
2023-02-08 15:22:00 +0100

    Adding hero

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,14 @@ import Feature from "../components/Feature";
 import Grid from "../components/Grid";
 import Page from "../components/Page";
 import Teaser from "../components/Teaser";
+import Hero from "../components/Hero";
 
 const components = {
   feature: Feature,
   grid: Grid,
   teaser: Teaser,
   page: Page,
+  hero: Hero,
 };
 
 storyblokInit({
