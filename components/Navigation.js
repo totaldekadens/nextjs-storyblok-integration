4059c5e092561830e86d80c9c124ea32df1b9cb0
totaldekadens <angelica.moberg@hotmail.com>
2023-02-08 16:07:45 +0100

    Parallell startup

--- components/Navigation.js
+++ components/Navigation.js
@@ -54,7 +54,7 @@ const Navigation = () => {
                 About
               </div>
             </Link>
-            <Link href="/blog">
+            <Link href="/blog-articles">
               <div className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Blog
               </div>
